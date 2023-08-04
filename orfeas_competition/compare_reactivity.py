bf871a63b598b2a34d7bf6f053199ed809f15010
Phil Travis <physicistphil@gmail.com>
2023-08-03 18:28:34 -0700

    Modified script for comparing reactivity parameterizations to work with new setup scripts.

--- orfeas_competition/compare_reactivity.py
+++ orfeas_competition/compare_reactivity.py
@@ -1,4 +1,55 @@
-import mirror_physics
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+# ## Values / JAX variables to optimize for
+values = {
+    'Eb': 1000.,
+    'beta': 0.8,
+    'B_pm': 30.,
+    'B_p': 6.,
+    'r_b': 0.25,
+    'L_p': 4.,
+    'p_aux': 0.0,  # this is in addition to synchotron losses (which are calculated separately)
+    'T_frac': 0.5,
+    'Tep_fudge': 0.5,  # this should remain constant
+
+    'B_cc': 1.5,
+    'n_ccr': 0.25,
+    'Ti_ccr': 1.0,
+    'Te_ccr': 1.0,
+    'L_cc': 20.0,
+
+    # efficiencies should remain constant
+    'Ef_DEC': 0.7,
+    'Ef_TE': 0.5,
+    'Ef_ECH': 0.6,
+    'Ef_NBI': 0.6,
+    'Ef_RF': 0.9,
+
+    # engineering parameters remaining constant (for now)
+    'd_blanket': 0.6,
+    'd_vv': 0.2,
+    'a_wall_r': 1.1,
+    'cost_HTS_kAm': 1e-4,  # in megadollars (M$)
+    'coil_spacing_cc': 1.0,  # in meters
+
+    # physical quantities that are remaining constant for simplicity
+    'mu': 2.5,
+    'Z_eff': 1.13,
+    'I_cooling': 0
+}
+
+
+# ## Flags
+flags = {
+    # assumes Ti_plug = 2/3 E_inj and Te_plug = 0.09 log(Rm/(sqrt(1-beta))^0.4)
+    'simple_temps': False,
+    'DD_cat': True,  # assumes DD fusion products are burned instantly. Is there any reason to turn this off?
+    'field_reg_alphas': False,  # Regularize midplane field strengths via an alpha particle gyroradii heuristic
+    'field_reg_Bohm': False,  # Regularize midplane field via Bohm diffusion heuristic
+    'cc_aux_ECH': True,  # Use ECH to replace tandem mirror axial losses
+}
+
+exec(open('mirror_physics_setup.py').read())
 
 # These constants are valid for 0.2-100 keV (Maxwellian) ion temperatures. Verified correct to 4 decimal places for 0.2, 1, and 10 keV.
 DT_BG = 34.3827  # keV^0.5
@@ -31,7 +82,7 @@ DD_nHe_C5 = -2.96400e-6
 DD_nHe_C6 = 0
 DD_nHe_C7 = 0
 
-DT_theta = Ti / (1 -  i * (DT_C2 + Ti * (DT_C4 + Ti * DT_C6)) / (1 + Ti * (DT_C3 + Ti * (DT_C5 + Ti * DT_C7))))
+DT_theta = Ti / (1 -  Ti * (DT_C2 + Ti * (DT_C4 + Ti * DT_C6)) / (1 + Ti * (DT_C3 + Ti * (DT_C5 + Ti * DT_C7))))
 DT_xi = (DT_BG ** 2 / (4 * DT_theta)) ** (1/3)
 DT_reactivity = DT_C1 * DT_theta * (DT_xi / (DT_mrc2 * Ti ** 3)) ** 0.5 * sympy.E ** (-3 * DT_xi) / 1e6
 
@@ -63,7 +114,6 @@ def calc_lin_reactivity(coefficients, x):
     x_mat = jnp.tri(num_c).transpose() + x_mat
     x = jnp.product(x_mat, axis=-2)
     return jnp.sum(x * coefficients, axis=1)
-
 
 
 # ## Cross section comparison
@@ -111,18 +161,15 @@ plt.legend()
 
 temp_plot = 10 ** jnp.linspace(0, 3, 100)
 plt.figure()
-
 plt.plot(temp_plot, calc_DT_reactivity_linear(temp_plot), label='DT')
 plt.plot(temp_plot, calc_DD_reactivity_linear(temp_plot), label='DD')
 plt.plot(temp_plot, calc_DHe3_reactivity_linear(temp_plot), label='D-He3')
-
 # plt.scatter(linear_reactivity_temps, linear_reactivity_DD, label="NRL datapoints")
-
 plt.yscale('log')
 plt.xscale('log')
 plt.title('DT, DD, and D-He3 fusion reactivity (log-space interpolation of NRL data)')
 plt.ylabel('Reactivity ($m^3/s$)')
 plt.xlabel("Temperature (keV)")
 plt.legend()
-
-plt.savefig('compare_reactivity.pdf')
+plt.show()
+# plt.savefig('compare_reactivity.pdf')
